ce789d8dc7f06abefc4bde6cfb6da5c87632de91
ankush <ankush.ivision@gmail.com>
2021-03-25 12:46:54 +0530

    project set up

---
+++ src/utils/Settings.js
@@ -0,0 +1,13 @@
+export const RESOLUTION_ARR = {
+  "120p,120p_1": [160, 120, 15, 65],
+  "360p_4": [640, 360, 30, 600],
+  "480p_4": [640, 480, 30, 750],
+  "720p_3": [1280, 720, 30, 1710]
+};
+
+export const SHARE_ID = 1;
+
+export const APP_ID = "4be4ffc903884cfda41af068e7aaf726";
+
+// Assign Token to null if you have not enabled app certificate
+export const Token = "0064be4ffc903884cfda41af068e7aaf726IADsluqusiNn4ygRZrbWPTrr/S6AnzEVPOpy3gxTxH04djLRTXgAAAAAEABfjXZEK4ddYAEAAQBjh11g";
